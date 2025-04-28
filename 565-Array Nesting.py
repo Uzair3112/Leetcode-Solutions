f55d64e3eebb74f09ad2ef1506dbdb2dbce8d944
willyboy437 <uzairtariq.pakistani@gmail.com>
2025-04-28 20:40:46 +0500

    565 added

---
+++ 565-Array Nesting.py
@@ -0,0 +1,12 @@
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        ans = 0
+        for x in nums:
+            if nums[x] == -1:
+                continue
+            count = 0
+            while nums[x] != -1:
+                count += 1
+                nums[x], x = -1, nums[x]
+            ans = max(count,ans)
+        return ans
