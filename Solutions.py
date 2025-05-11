b6064776ea5fb8f41b19d5b0dbc59c75bc969900
willyboy437 <uzairtariq.pakistani@gmail.com>
2025-05-11 23:07:45 +0500

    2570 Added

--- Solutions.py
+++ Solutions.py
@@ -77,3 +77,30 @@ class Solution:
             if len(set(i)) == len(set(nums)):
                 ans += 1
         return ans
+
+
+#2570. Merge Two 2D Arrays by Summing Values
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        ans = []
+        n1 = len(nums1)
+        n2 = len(nums2)
+        i, j = 0, 0
+        while i < n1 and j < n2:
+            if nums1[i][0] == nums2[j][0]:
+                ans.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+            elif nums1[i][0] < nums2[j][0]:
+                ans.append(nums1[i])
+                i += 1
+            else: 
+                ans.append(nums2[j])
+                j += 1
+        while i < n1:
+            ans.append(nums1[i])
+            i += 1
+        while j < n2:
+            ans.append(nums2[j])
+            j += 1
+        return ans
