ae2a14192b0f68a844a4118eaa1a7494e22e5831
Uzair3112 <uzairtariq.pakistani@gmail.com>
2025-05-21 01:40:56 +0500

    1534 Added

--- Solutions.py
+++ Solutions.py
@@ -199,3 +199,16 @@ class Solution:
                 t = True
                 return t
         return t
+
+#1534. Count Good Triplets
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        count = 0
+        for i in range(len(arr)-2):
+            for j in range(i+1,len(arr)-1):
+                for k in range(j+1,len(arr)):
+                    if abs(arr[i]-arr[j]) <= a:
+                        if abs(arr[j]-arr[k]) <= b:
+                            if abs(arr[i]-arr[k]) <= c:
+                                count += 1
+        return count
